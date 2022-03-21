e15f69319c1f663498a570220af938010e568a6f
Horacio <ackerman@LAPTOP-QNFEQB2Q.localdomain>
2022-03-21 15:27:28 -0300

    Figurasjs modificacion

--- figuras.js
+++ figuras.js
@@ -1,4 +1,5 @@
 //Codigo del cuadrado
+console.group("Cuadrados");
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
 
@@ -7,15 +8,46 @@ console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
+console.groupEnd();
 
 //Codigo del triangulo
+console.group("Triangulos");
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
 console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
+console.log("La altura del triangulo mide: " + alturaTriangulo + "cm");
+
 
 const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
 console.log("El perímetro del triangulo es: " + perimetroTriangulo + "cm");
 
-const areaTriangulo = baseTriangulo;
-console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triangulo es: " + areaTriangulo + "cm2");
+console.groupEnd();
+
+//Codigo del Circulo
+console.group("Circulos");
+
+//Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es: " + radioCirculo + "cm");
+//Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es: " + diametroCirculo + "cm");
+
+//PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+//Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perimetro del circulo es: " + perimetroCirculo + "cm");
+
+//Area
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El area del circulo es: " + areaCirculo + "cm2");
+
+
+console.groupEnd();
