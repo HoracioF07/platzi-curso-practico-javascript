e00a1fb072564c5f731c3be4de08c3ae87fc54e6
Horacio <ackerman@LAPTOP-QNFEQB2Q.localdomain>
2022-03-21 13:20:34 -0300

    Figuras html y js

---
+++ figuras.js
@@ -0,0 +1,21 @@
+//Codigo del cuadrado
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
+
+//Codigo del triangulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triangulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = baseTriangulo;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
